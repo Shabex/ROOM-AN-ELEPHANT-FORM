fc32cb7de41740802d2e05aae44a4ebe8c296147
Shabex <shabsibrah@gmail.com>
2022-06-23 14:49:29 +0300

    adds changes in the files

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 
-document.addEventListener('DOMContentLoaded',() => {
-    
-    function getElephant() {
+document.addEventListener("DOMContentLoaded",() => {
+     const navBar = document.getElementById("elephant-list")
 
-        fetch('https://flame-somber-crepe.glitch.me/db.json')
-        .then(res=>res/json())
-        .then(data => {
-            return elephantSpecies(data);
-        }) 
-
-    }
-
-    getElephant();
-
-  function elephantSpecies(elephants){
-      elephants.forEach(elephant => {
-          const barNav = document.getElementById('elephant-list')
-          const list = document.createElement('li')
-
-          list.textContent = `${elephant.name}`
-          barNav.appendeChild(list)
-      })
-           
-      }
-  
+     fetch("")
 })
+
+  
+
