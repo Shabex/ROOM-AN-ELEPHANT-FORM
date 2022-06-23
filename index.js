5d70e4b9a66cec6e2906783e0ec60588f16ca0be
Shabex <shabsibrah@gmail.com>
2022-06-23 12:21:52 +0300

    added db.json file

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 
 document.addEventListener('DOMContentLoaded',() => {
-    console.log('project proposal')
+    
+    function getElephant() {
+
+        fetch('https://elephant-api.herokuapp.com/elephants')
+        .then(res=>res/json())
+        .then(data => {
+            return elephantSpecies(data);
+        }) 
+
+    }
+
+    getElephant();
+
+  function elephantSpecies(elephants){
+      elephants.forEach(elephant => {
+          const barNav = document.getElementById('elephant-list')
+          const list = document.createElement('li')
+
+          list.textContent = `${elephant.name}`
+          barNav.appendeChild(list)
+      })
+           
+      }
+  
 })
